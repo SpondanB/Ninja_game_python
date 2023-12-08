d5085c5265f6ca9441c05e81f56c4256ce1fafa0
SpondanB <spondan2003@gmail.com>
2023-12-08 22:45:40 +0530

    Tilemap added

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self) -> None:
@@ -9,22 +10,29 @@ class Game:
         pygame.display.set_caption("Ninja Game")
         
         self.screen = pygame.display.set_mode((640,480))
+        self.display = pygame.Surface((320, 240))
+
         self.clock = pygame.time.Clock()
 
         self.movement = [False, False]
 
         self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png')
         }
         
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.tilemap = Tilemap(self, tile_size=16)
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
-            
+            self.display.fill((14, 219, 248))
+            self.tilemap.render(self.display)
             self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -40,7 +48,8 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-
+            
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
