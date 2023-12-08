e56ef1796a50405d54859f5adfcdb04f19b0fc4e
SpondanB <spondan2003@gmail.com>
2023-12-08 10:27:30 +0530

    Added image to the screen/surface and made it move

--- game.py
+++ game.py
@@ -1,19 +1,42 @@
 import pygame
 import sys
 
-pygame.init()
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.display.set_caption("Ninja Game")
+        
+        self.screen = pygame.display.set_mode((640,480))
+        self.clock = pygame.time.Clock()
 
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))   # removes the black background from the image making it transparent
+        self.img_pos = [160,250]
+        self.movement = [False, False]
 
-pygame.display.set_caption("Ninja Game")
-screen = pygame.display.set_mode((640,480))
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
 
-clock = pygame.time.Clock()
+            self.screen.blit(self.img, self.img_pos)
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
 
-    pygame.display.update()
-    clock.tick(60)
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
